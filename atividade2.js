8c8aa0bf9df62a46be2d4a7cb9af90dc8c2e7d92
SaymonM <saymonmeura@hotmail.com>
2021-08-26 19:46:44 -0400

    operadores logicos

--- atividade2.js
+++ atividade2.js
@@ -1,15 +1,34 @@
 let numDeAlunos = 10;
-let contador = 0;
+// let contador = 0;
 
-do {
-    if (contador == 0) {
-        console.log("O número é zero!!");
-    } else if (contador % 2 != 0){
-        console.log("O número é impar: "+ contador);
-    } else
-    console.log("O número é par: "+ contador);
-    contador++;
-} while (contador <= numDeAlunos);
+for (let contador = 0; contador <= numDeAlunos; contador++) {
+    
+    if (contador == 0){
+        console.log("Número é zero!!");
+    }
+    else if (contador % 2 == 0 && contador > 5) {
+       console.log("Número Par: "+ contador +" Maior que 5"); 
+    }
+    else{
+        console.log("Número Impar: "+ contador);
+    }  
+}
+
+let usuario;
+
+if(!usuario){
+    console.log("Resposta vazia!!");
+}
+
+// do {
+//     if (contador == 0) {
+//         console.log("O número é zero!!");
+//     } else if (contador % 2 != 0){
+//         console.log("O número é impar: "+ contador);
+//     } else
+//     console.log("O número é par: "+ contador);
+//     contador++;
+// } while (contador <= numDeAlunos);
 
 // while (contador <= numDeAlunos) {
 //     if (contador == 0) {
